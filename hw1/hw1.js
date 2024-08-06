baee78595685f2d340da3318b60f465b08381654
x <fedorenkomcbook@gmail.com>
2024-08-06 12:17:15 +0200

    Initial commit

---
+++ hw1/hw1.js
@@ -0,0 +1,60 @@
+// task1
+let greeting = 'hello';
+let school = 'owu';
+let domain = 'com';
+let country = 'ua';
+let one = 1;
+let ten = 10;
+let negativeNumber = -999;
+let positiveNumber = 123;
+let pi = 3.14;
+let euler = 2.7;
+let sixteen = 16;
+let isTrue = true;
+let isFalse = false;
+
+console.log(greeting);
+console.log(school);
+console.log(domain);
+console.log(country);
+console.log(one);
+console.log(ten);
+console.log(negativeNumber);
+console.log(positiveNumber);
+console.log(pi);
+console.log(euler);
+console.log(sixteen);
+console.log(isTrue);
+console.log(isFalse);
+
+//task2
+let firstName = 'Oleksandr';
+let middleName = 'Serhiyovych';
+let lastName = 'Fedorenko';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+
+console.log(person);
+
+//task3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a); // number
+console.log(typeof b); // string
+console.log(typeof c); // boolean
+//task 4
+
+let firstNamee = prompt("Введіть ваше ім'я:");
+let middleNamee = prompt("Введіть ваше по батькові:");
+let age = prompt("Введіть ваш вік:");
+
+
+console.log("Ім'я: " + firstNamee);
+console.log("По батькові: " + middleNamee);
+console.log("Вік: " + age);
+
+
+
